3f119142fa9bdd7c4bb3202167b754988e4058d7
1nt3rnal3rr0r <18672357+1nt3rnal3rr0r@users.noreply.github.com>
2018-03-19 15:45:48 +0200

    added initial code

---
+++ controller/start.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import threading
+import queue
+
+app = Flask(__name__)
+messages = queue.Queue()
+CORS(app)
+
+
+def print_messages():
+    threading.Timer(1, print_messages).start()
+    while not messages.empty():
+        print(messages.get())
+
+
+@app.route('/message', methods=['POST'])
+def message():
+    data = request.get_json()
+    if data and 'message' in data and len(data['message']) > 0:
+        messages.put(data['message'])
+        return jsonify({'success': True, 'message': 'Message received.'})
+    else:
+        return jsonify({'success': False, 'message': 'Invalid request.'})
+
+
+@app.route('/')
+def hello():
+    return 'Hello!'
+
+
+if __name__ == "__main__":
+    print_messages()
+    app.run(host='0.0.0.0', port=4000, threaded=True)
